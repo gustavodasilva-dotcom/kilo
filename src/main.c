e29c0bcf198ab5601f0b50f01c50ec1ca6e6f8d7
Gustavo Aquino da Silva <gustavaquinooficial@gmail.com>
2025-11-09 10:43:23 -0300

    feat: add horizontal scrolling

--- src/main.c
+++ src/main.c
@@ -39,6 +39,7 @@ struct editorConfig
 {
     int cx, cy;
     int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -327,12 +328,15 @@ void editorDrawRows(struct abuf *ab)
         }
         else
         {
-            int len = E.row[filerow].size;
+            int len = E.row[filerow].size - E.coloff;
+
+            if (len < 0)
+                len = 0;
 
             if (len > E.screencols)
                 len = E.screencols;
 
-            abAppend(ab, E.row[filerow].chars, len);
+            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -355,6 +359,16 @@ void editorScroll()
     {
         E.rowoff = E.cy - E.screenrows + 1;
     }
+
+    if (E.cx < E.coloff)
+    {
+        E.coloff = E.cx;
+    }
+
+    if (E.cx >= E.coloff + E.screencols)
+    {
+        E.coloff = E.cx - E.screencols + 1;
+    }
 }
 
 void editorRefreshScreen()
@@ -371,7 +385,7 @@ void editorRefreshScreen()
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     // Show the cursor again after finishing drawing.
@@ -396,10 +410,7 @@ void editorMoveCursor(int key)
         }
         break;
     case ARROW_RIGHT:
-        if (E.cx != E.screencols - 1)
-        {
-            E.cx++;
-        }
+        E.cx++;
         break;
     case ARROW_UP:
         if (E.cy != 0)
@@ -461,6 +472,7 @@ void initEditor()
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
 
