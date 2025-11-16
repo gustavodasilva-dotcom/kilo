2a22a9f06c972cdfd82b6e9958007b7f75848d1f
Gustavo Aquino da Silva <gustavaquinooficial@gmail.com>
2025-11-16 13:40:59 -0300

    feat: add c file format highlight / add string highlight

--- src/main.c
+++ src/main.c
@@ -34,7 +34,18 @@ enum editorKey {
     PAGE_DOWN
 };
 
-enum editorHighlight { HL_NORMAL = 0, HL_NUMBER, HL_MATCH };
+enum editorHighlight { HL_NORMAL = 0, HL_STRING, HL_NUMBER, HL_MATCH };
+
+#define HL_HIGHLIGHT_NUMBERS (1 << 0)
+#define HL_HIGHLIGHT_STRINGS (1 << 1)
+
+// data
+
+struct editorSyntax {
+    char *filetype;
+    char **filematch;
+    int flags;
+};
 
 typedef struct erow {
     int size;
@@ -58,10 +69,26 @@ struct editorConfig {
     char *filename;
     char statusmsg[80];
     time_t statusmsg_time;
+    struct editorSyntax *syntax;
     struct termios orig_termios;
 };
 
 struct editorConfig E;
+
+// data
+
+// filetypes
+
+char *C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
+
+// highlight database
+struct editorSyntax HLDB[] = {
+    {"c", C_HL_extensions, HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS},
+};
+
+#define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
+
+// filetypes
 
 // prototypes
 
@@ -215,8 +242,11 @@ void editorUpdateSyntax(erow *row) {
     // Highlights all the text to normal by default.
     memset(row->hl, HL_NORMAL, row->rsize);
 
+    if (E.syntax == NULL) return;
+
     // Starts as true, considering the beginning of the line as a separator.
     int prev_sep = 1;
+    int in_string = 0;
 
     int i = 0;
     while (i < row->rsize) {
@@ -224,12 +254,56 @@ void editorUpdateSyntax(erow *row) {
 
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
-        if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
-            (c == '.' && prev_hl == HL_NUMBER)) {
-            row->hl[i] = HL_NUMBER;
-            i++;
-            prev_sep = 0;
-            continue;
+        if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
+            // Checks if it's currently in a string (meaning, its value is either a double-quote or
+            // a single-quote character).
+            if (in_string) {
+                row->hl[i] = HL_STRING;
+
+                // If the current character is a backslash and there is at least one more character
+                // after it, highlights that character as a string (handling escaped strings).
+                if (c == '\\' && i + 1 < row->rsize) {
+                    row->hl[i + 1] = HL_STRING;
+                    // Consumes the backslash and the string character.
+                    i += 2;
+                    continue;
+                }
+
+                // If the current character is the closing quote, it denotes the string is
+                // completed, resetting the control variables.
+                if (c == in_string) in_string = 0;
+
+                // Consumes the character.
+                i++;
+                // Considers the quote a separator.
+                prev_sep = 1;
+
+                continue;
+            } else {
+                if (c == '"' || c == '\'') {
+                    // Stores the quote.
+                    in_string = c;
+
+                    row->hl[i] = HL_STRING;
+
+                    // Consumes the character.
+                    i++;
+
+                    continue;
+                }
+            }
+        }
+
+        if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
+            if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
+                (c == '.' && prev_hl == HL_NUMBER)) {
+                row->hl[i] = HL_NUMBER;
+
+                i++;
+                prev_sep = 0;
+
+                continue;
+            }
         }
 
         prev_sep = is_separator(c);
@@ -239,12 +313,43 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl) {
     switch (hl) {
+        case HL_STRING:
+            return 35;
         case HL_NUMBER:
             return 31;
         case HL_MATCH:
             return 34;
         default:
             return 37;
+    }
+}
+
+void editorSelectSyntaxHighlight() {
+    E.syntax = NULL;
+    if (E.filename == NULL) return;
+
+    char *ext = strrchr(E.filename, '.');
+
+    for (unsigned int j = 0; j < HLDB_ENTRIES; j++) {
+        struct editorSyntax *s = &HLDB[j];
+
+        unsigned int i = 0;
+
+        while (s->filematch[i]) {
+            int is_ext = (s->filematch[j][0] == '.');
+            if ((is_ext && ext && !strcmp(ext, s->filematch[i])) ||
+                (!is_ext && strstr(E.filename, s->filematch[i]))) {
+                E.syntax = s;
+
+                int filerow;
+                for (filerow = 0; filerow < E.numrows; filerow++) {
+                    editorUpdateSyntax(&E.row[filerow]);
+                }
+
+                return;
+            }
+            i++;
+        }
     }
 }
 
@@ -515,6 +620,8 @@ void editorOpen(char *filename) {
     free(E.filename);
     E.filename = strdup(filename);
 
+    editorSelectSyntaxHighlight();
+
     FILE *fp = fopen(filename, "r");
     if (!fp) die("fopen");
 
@@ -542,6 +649,7 @@ void editorSave() {
             editorSetStatusMessage("Save aborted");
             return;
         }
+        editorSelectSyntaxHighlight();
     }
 
     int len;
@@ -759,7 +867,8 @@ void editorDrawStatusBar(struct abuf *ab) {
     int len =
         snprintf(status, sizeof(status), "%.20s - %d lines %s",
                  E.filename ? E.filename : "[No Name]", E.numrows, E.dirty ? "(modified)" : "");
-    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%s | %d/%d",
+                        E.syntax ? E.syntax->filetype : "no ft", E.cy + 1, E.numrows);
 
     if (len > E.screencols) len = E.screencols;
 
@@ -1032,6 +1141,7 @@ void initEditor() {
     E.filename = NULL;
     E.statusmsg[0] = '\0';
     E.statusmsg_time = 0;
+    E.syntax = NULL;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 
